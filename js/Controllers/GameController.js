1fcf3dc692a19adc083ae918d01bc0a80d8c7566
Filipe Santos Correa <filipe.santoscorrea@live.de>
2015-01-13 13:01:03 +0100

    Splitted game interval in two functions: 'controlGame' and 'draw'. In the draw function 'window.requestAnimationFrame' is called for a better drawing performance.

--- js/Controllers/GameController.js
+++ js/Controllers/GameController.js
@@ -16,67 +16,59 @@ define( ['Models/Snake', 'Models/Rules', 'Models/Playground', 'Views/PlaygroundV
 
         //generate food, for game start
         playground.generateFood(playground.playground, snake.snake);
+        controlGame();
 
-        var timerId = setInterval(function() {
 
-            if (rules.isSnakeAlive(snake.snake, playground.playground)) {
-                document.onkeydown = function(e) {
-                    switch (e.keyCode) {
-                        case 37:
-                            snake.validateDirection('left');
-                            break;
-                        case 38:
-                            snake.validateDirection('up');
-                            break;
-                        case 39:
-                            snake.validateDirection('right');
-                            break;
-                        case 40:
-                            snake.validateDirection('bottom');
-                            break;
+        function controlGame() {
+
+            var timerId = setInterval(function() {
+
+                draw();
+
+                if (rules.isSnakeAlive(snake.snake, playground.playground)) {
+                    document.onkeydown = function(e) {
+                        switch (e.keyCode) {
+                            case 37:
+                                snake.validateDirection('left');
+                                break;
+                            case 38:
+                                snake.validateDirection('up');
+                                break;
+                            case 39:
+                                snake.validateDirection('right');
+                                break;
+                            case 40:
+                                snake.validateDirection('bottom');
+                                break;
+                        }
+                    };
+                    snake.controlSnake(snake.lastDirection);
+
+                    if (playground.didSnakeAteFood(snake.snake)) {
+                        snake.inkrementSnake();
+                        playground.generateFood(playground.playground, snake.snake);
                     }
-                };
 
-                snake.controlSnake(snake.lastDirection);
-
-                playgroundView.clearPlayground();
-                playgroundView.drawSnake(snake.snake);
-
-
-                if (playground.didSnakeAteFood(snake.snake)) {
-                    // refresh view
-                    playgroundView.snakeAteFood(snake.snake, playground.food);
-
-                    snake.inkrementSnake();
-
-
-                    playground.generateFood(playground.playground, snake.snake);
-                    playgroundView.displayFood(playground.food);
                 } else {
-                    playgroundView.displayFood(playground.food);
+
+                    if (playgroundView.displayGameOver()) {
+                        snake = new Snake(playground.playground);
+                        rules = new Rules(true);
+                    } else {
+                        clearInterval(timerId);
+                    }
                 }
 
-            } else {
+            }, 50);
+        }
 
-                if (playgroundView.displayGameOver()) {
-                    snake = new Snake(playground.playground);
-                    rules = new Rules(true);
-                } else {
-                    clearInterval(timerId);
-                }
-            }
+        function draw() {
+            window.requestAnimationFrame(draw);
 
-
-            // measure FPS
-            current ++;
-            if (Date.now() - lastUpdated >= 1000) {
-                console.log(current);
-                current = 0;
-                lastUpdated = Date.now();
-            }
-
-        }, 50);
-
+            playgroundView.clearPlayground();
+            playgroundView.drawSnake(snake.snake);
+            playgroundView.displayFood(playground.food);
+        }
     }
 
     return {
