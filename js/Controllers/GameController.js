159e2ab266eaf56a10dd2bd8e889bea1127c80f3
Filipe Santos Correa <filipe.santoscorrea@live.de>
2015-01-14 15:22:31 +0100

    Added polyfill for 'requestAnimationFrame'. Added Touch events and method to detect if device has a touchscreen.

--- js/Controllers/GameController.js
+++ js/Controllers/GameController.js
@@ -9,6 +9,16 @@ define( ['Models/Snake', 'Models/Rules', 'Models/Playground', 'Views/PlaygroundV
         var snake = new Snake(playground.playground);
         var rules = new Rules(true);
 
+        if (is_touch_device()) {
+            document.addEventListener('touchstart', handleTouchStart, false);
+            document.addEventListener('touchmove', handleTouchMove, false);
+
+            var xDown = null;
+            var yDown = null;
+        } else {
+            document.addEventListener('keydown', keydown, false);
+        }
+
 
         //generate food, for game start
         playground.generateFood(playground.playground, snake.snake);
@@ -22,22 +32,6 @@ define( ['Models/Snake', 'Models/Rules', 'Models/Playground', 'Views/PlaygroundV
 
                 if (rules.isSnakeAlive(snake.snake, playground.playground)) {
 
-                    document.onkeydown = function(e) {
-                        switch (e.keyCode) {
-                            case 37:
-                                snake.validateDirection('left');
-                                break;
-                            case 38:
-                                snake.validateDirection('up');
-                                break;
-                            case 39:
-                                snake.validateDirection('right');
-                                break;
-                            case 40:
-                                snake.validateDirection('bottom');
-                                break;
-                        }
-                    };
                     snake.controlSnake(snake.lastDirection);
 
                     if (playground.didSnakeAteFood(snake.snake)) {
@@ -58,6 +52,12 @@ define( ['Models/Snake', 'Models/Rules', 'Models/Playground', 'Views/PlaygroundV
             }, 50);
         }
 
+
+
+
+
+
+
         function draw() {
             window.requestAnimationFrame(draw);
 
@@ -67,6 +67,63 @@ define( ['Models/Snake', 'Models/Rules', 'Models/Playground', 'Views/PlaygroundV
             playgroundView.drawSnake(snake.snake);
             playgroundView.displayFood(playground.food);
         }
+
+        function is_touch_device() {
+            return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+        }
+
+        function handleTouchStart(evt) {
+            xDown = evt.touches[0].clientX;
+            yDown = evt.touches[0].clientY;
+        }
+
+        function handleTouchMove(evt) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+            evt.preventDefault();
+
+            var xUp = evt.touches[0].clientX;
+            var yUp = evt.touches[0].clientY;
+
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                if ( xDiff > 0 ) {
+                    snake.validateDirection('left');
+                } else {
+                    snake.validateDirection('right');
+                }
+            } else {
+                if ( yDiff > 0 ) {
+                    snake.validateDirection('up');
+                } else {
+                    snake.validateDirection('bottom');
+                }
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;
+        }
+
+        function keydown(evt) {
+            switch (evt.keyCode) {
+                case 37:
+                    snake.validateDirection('left');
+                    break;
+                case 38:
+                    snake.validateDirection('up');
+                    break;
+                case 39:
+                    snake.validateDirection('right');
+                    break;
+                case 40:
+                    snake.validateDirection('bottom');
+                    break;
+            }
+        }
+
     }
 
     return {
