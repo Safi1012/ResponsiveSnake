e737a9b0539981a6d45ffda24be8428b5bdefcfa
Filipe Santos Correa <filipe.santoscorrea@live.de>
2015-01-18 12:33:30 +0100

    Added 'manifest' task to grunt. Now 'manifest.appcache' get generated after each file modification.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-manifest');
 
 	var LIVERELOAD_PORT = 35792;
 
@@ -15,24 +16,62 @@ module.exports = function (grunt) {
 				}
 			}
 		},
+		manifest: {
+			generate: {
+				options: {
+					basePath: '/',
+					cache: [
+						// 'js/', or wildcard ?
+						'stylesheet.css',
+						'manifest.json',
+						'img/favicon.ico',
+
+						'js/Controllers/GameController.js',
+						'js/Models/Playground.js',
+						'js/Models/Rules.js',
+						'js/Models/Score.js',
+						'js/Models/Snake.js',
+						'js/Views/PlaygroundView.js',
+						'js/main.js',
+						'js/require.js'
+					],
+					network: ['http://*', 'https://*'],
+					fallback: ['/ /index.html'],
+					// exclude: ['js/jquery.min.js'],
+					preferOnline: true,
+					verbose: true,
+					timestamp: true,
+					hash: true,
+					master: ['index.html']
+				},
+				src: [
+					'index.html',
+					'js/',
+					'img/',
+					'stylesheet.css',
+					'manifest.json'
+				],
+				dest: 'manifest.appcache'
+			}
+		},
 		watch: {
 			scss: {
 				files: ['scss/*.scss'],
-				tasks: ['sass:dev'],
+				tasks: ['sass:dev', 'manifest:generate'],
 				options: {
 					livereload: LIVERELOAD_PORT
 				}
 			},
 			html: {
 				files: ['index.html'],
-				tasks: [],
+				tasks: ['manifest:generate'],
 				options: {
 					livereload: LIVERELOAD_PORT
 				}
 			},
             js: {
                 files: ['js/**/*.js'],
-                tasks: [],
+                tasks: ['manifest:generate'],
 				options: {
 					livereload: LIVERELOAD_PORT
 				}
@@ -52,6 +91,7 @@ module.exports = function (grunt) {
 		}
 	});
 
-	grunt.registerTask("serve", ["sass", "connect", "watch"]);
+	grunt.registerTask("serve", ["manifest", "sass", "connect", "watch"]);
 	grunt.registerTask("default", ["serve"]);
+	grunt.registerTask("generate", ["manifest"]);
 };
